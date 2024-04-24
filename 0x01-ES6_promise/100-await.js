81d7d6db86f07152760f8af87f42a2008ecda93e
Mihretu1985 <mthaile1985@gmail.com>
2024-04-23 23:49:16 -0700

    updated

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,28 @@
-import { uploadPhoto, createUser } from './utils';
+// Example of using async/await with promises
 
-export default async function asyncUploadUser() {
-  let res = {};
-
-  try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    res = { photo, user };
-  } catch (err) {
-    res = { photo: null, user: null };
-  }
-  return res;
+// Function to simulate an asynchronous operation
+function fetchUserData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const userData = { id: 1, username: 'Mihretu1985', email: 'mthaile1985@gmail.com' };
+      // Simulating a successful API response
+      resolve(userData);
+      // Simulating an error
+      // reject(new Error('Failed to fetch user data'));
+    }, 2000); // Simulating a delay of 2 seconds
+  });
 }
+
+// Function to fetch user data using async/await
+async function getUserData() {
+  try {
+    console.log('Fetching user data...');
+    const userData = await fetchUserData();
+    console.log('User data:', userData);
+  } catch (error) {
+    console.error('Error fetching user data:', error.message);
+  }
+}
+
+// Calling the getUserData function
+getUserData();
--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
-
-rt default function handleResponseFromAPI(promise) {
+/* eslint-disable no-console */
+export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error())
-    .finally(() => console.log('Got a response from the API'));
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
